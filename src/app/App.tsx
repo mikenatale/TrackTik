3c7b0b13dde17a42d498d83db712983772862184
Mike Natale <mnatale@streamsystems.ca>
2019-09-28 15:42:15 -0400

    chore: add typescript+scss + restructure project

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import './App.scss';
+
+export default function App(): ReactElement {
+  return (
+    <div>Hello world except with TypeScript</div>
+  );
+}
