997d40c9b81206ba92d8a8641a7a18ccc68d2acf
Mike Natale <mnatale@streamsystems.ca>
2019-09-28 16:34:41 -0400

    feat: update app root to prep for home page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,38 @@
 import React, { ReactElement } from 'react';
+import { Grid } from '@material-ui/core';
 
-// TODO possibly remove
-import './App.scss';
+import AppHeader from 'app/components/AppHeader/AppHeader';
+import HomePage from 'app/pages/HomePage';
 
-import AppHeader from './components/AppHeader/AppHeader';
+interface IState {
+  selectedSite: null;
+}
 
-export default function App(): ReactElement {
-  return (
-    <AppHeader />
-  );
+export default class App extends React.Component<{}, IState> {
+  constructor() {
+    super({});
+
+    this.state = {
+      selectedSite: null
+    };
+  }
+
+  render(): ReactElement {
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <AppHeader />
+        </Grid>
+        <Grid item xs={12}>
+          {
+            this.state.selectedSite
+            ?
+              <></>
+            :
+              <HomePage />
+          }
+        </Grid>
+      </Grid>
+    );
+  }
 }
